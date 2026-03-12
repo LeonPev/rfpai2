ff1561de181d616c7a3b0ebe4db0a4534eb07d73
leon <leonpevzner@gmail.com>
2026-03-12 13:18:53 +0200

    storage

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ gcloud run deploy rfpai \
   --memory 1G \
   --cpu 1 \
   --timeout=1000 \
-  --set-env-vars="GEMINI_API_KEY=AIzaSyCioACxvlUHemWSuQ0DlFBKMiySpsfE5-c,WORKFLOW_BUCKET=rfpai-jobs,WORKFLOW_PREFIX=workflows,FILE_BUCKET=rfpai-files,FILE_PREFIX=files,FILE_LOCAL_FALLBACK=false"
+  --set-env-vars="GEMINI_API_KEY=aaa,WORKFLOW_BUCKET=rfpai-jobs,WORKFLOW_PREFIX=workflows,FILE_BUCKET=rfpai-files,FILE_PREFIX=files,FILE_LOCAL_FALLBACK=false"
